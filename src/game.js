fdbd1fc16ff82a45c626d4542a9808a1a4caa34b
DeividasRo <slenderiss@gmail.com>
2022-12-21 01:59:16 +0200

    almost done

--- src/game.js
+++ src/game.js
@@ -3,28 +3,34 @@ const playerX = { name: "PlayerX", score: 0 };
 const playerO = { name: "PlayerO", score: 0 };
 let drawScore = 0;
 
-if (sessionStorage.playerX_data) {
-    playerX.name = JSON.parse(sessionStorage.playerX_data).name;
-    playerX.score = JSON.parse(sessionStorage.playerX_data).score;
-}
-if (sessionStorage.playerO_data) {
-    playerO.name = JSON.parse(sessionStorage.playerO_data).name;
-    playerO.score = JSON.parse(sessionStorage.playerO_data).score;
-}
-if (sessionStorage.draw_score) {
-    drawScore = JSON.parse(sessionStorage.draw_score);
+// check if game is not setup
+if (sessionStorage.playerX_data && sessionStorage.playerO_data) {
+    document.getElementById("game-setup-container").classList.add('hidden');
+    document.getElementById("game").classList.remove('hidden');
+    if (sessionStorage.playerX_data) {
+        playerX.name = JSON.parse(sessionStorage.playerX_data).name;
+        playerX.score = JSON.parse(sessionStorage.playerX_data).score;
+    }
+    if (sessionStorage.playerO_data) {
+        playerO.name = JSON.parse(sessionStorage.playerO_data).name;
+        playerO.score = JSON.parse(sessionStorage.playerO_data).score;
+    }
+    if (sessionStorage.draw_score) {
+        drawScore = JSON.parse(sessionStorage.draw_score);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+    updateTurnText(playerX);
+    updateNameTexts();
     updateScoreTexts();
 });
-
-console.log(playerX.score, drawScore, playerO.score);
 
 const game = {
     xTurn: true,
     oState: [],
     xState: [],
+    finished: false,
     winningStates: [
         ['0', '1', '2'],
         ['3', '4', '5'],
@@ -39,6 +45,17 @@ const game = {
     ]
 };
 
+function startGame() {
+    playerX.name = document.getElementsByName("playerX-name")[0].value
+    playerO.name = document.getElementsByName("playerO-name")[0].value;
+    sessionStorage.playerX_data = JSON.stringify(playerX);
+    sessionStorage.playerO_data = JSON.stringify(playerO);
+    document.getElementById("game-setup-container").classList.add('hidden');
+    document.getElementById("game").classList.remove('hidden');
+    updateTurnText(playerX);
+    updateNameTexts();
+}
+
 document.addEventListener('click', event => {
     const target = event.target
     const isCell = target.classList.contains('grid-cell')
@@ -47,8 +64,13 @@ document.addEventListener('click', event => {
     if (isCell && !isDisabled) {
         const cellValue = target.dataset.value;
 
-        game.xTurn === true ?
-            game.xState.push(cellValue) : game.oState.push(cellValue);
+        if (game.xTurn === true) {
+            game.xState.push(cellValue);
+            updateTurnText(playerO);
+        } else {
+            game.oState.push(cellValue);
+            updateTurnText(playerX);
+        }
 
         target.classList.add('disabled');
         target.classList.add(game.xTurn ? 'x' : 'o');
@@ -57,22 +79,15 @@ document.addEventListener('click', event => {
 
         game.xTurn = !game.xTurn;
 
-        if (!document.querySelectorAll('.grid-cell:not(.disabled)').length) {
-            document.querySelector('.game-over').classList.add('visible');
-            document.querySelector('.game-over-text').textContent = 'Draw!';
-            drawScore += 1;
-            sessionStorage.draw_score = drawScore;
-        }
-
         game.winningStates.forEach(winningState => {
             const xWins = winningState.every(state => game.xState.includes(state));
             const oWins = winningState.every(state => game.oState.includes(state));
-
+            console.log("winningstates");
             if (xWins || oWins) {
                 document.querySelectorAll('.grid-cell').forEach(cell => cell.classList.add('disabled'));
                 document.querySelector('.game-over').classList.add('visible');
                 document.querySelector('.game-over-text').textContent = xWins ?
-                    'X wins!' : 'O wins!';
+                    playerX.name + ' wins!' : playerO.name + ' wins!';
 
                 if (xWins) {
                     playerX.score += 1;
@@ -82,8 +97,18 @@ document.addEventListener('click', event => {
                     playerO.score += 1;
                     sessionStorage.playerO_data = JSON.stringify(playerO);
                 }
+                game.finished = true;
             }
         });
+
+        if (!document.querySelectorAll('.grid-cell:not(.disabled)').length && !game.finished) {
+            document.querySelector('.game-over').classList.add('visible');
+            document.querySelector('.game-over-text').textContent = 'Draw!';
+            drawScore += 1;
+            sessionStorage.draw_score = drawScore;
+            game.finished = true;
+            console.log("draw");
+        }
     }
 
 });
@@ -94,19 +119,39 @@ document.querySelector('.restart').addEventListener('click', () => {
         cell.classList.remove('disabled', 'x', 'o');
         cell.querySelector('img').removeAttribute("src");
     })
+
     updateScoreTexts();
 
-    game.xTurn = true;
+    game.finished = false;
     game.xState = [];
     game.oState = [];
-})
+});
+
+
+function resetScores() {
+    playerO.score = 0;
+    playerX.score = 0;
+    drawScore = 0
+    sessionStorage.draw_score = drawScore;
+    sessionStorage.playerO_data = JSON.stringify(playerO);
+    sessionStorage.playerX_data = JSON.stringify(playerX);
+    updateScoreTexts();
+}
 
 
 function updateScoreTexts() {
-
     document.getElementById("draw-info").querySelector('.player-score').textContent = drawScore;
     document.getElementById("playerX-info").querySelector('.player-score').textContent = playerX.score;
     document.getElementById("playerO-info").querySelector('.player-score').textContent = playerO.score;
+}
+
+function updateNameTexts() {
+    document.getElementById("playerX-info").querySelector('.player-name').textContent = playerX.name;
+    document.getElementById("playerO-info").querySelector('.player-name').textContent = playerO.name;
+}
+
+function updateTurnText(player) {
+    document.getElementById("turn").textContent = player.name + '\'s turn';
 }
 
 
